36e2d15989c569de7e6b92ea4ef42ef564ca130a
marcosmatosteodoro <mp.matos25@gmail.com>
2023-03-25 12:26:12 -0300

    Novas rotas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+
+
+Route::resources(['categories' => 'App\Http\Controllers\CategoryController']);
+Route::resources(['activity' => 'App\Http\Controllers\ActivityController']);
+
