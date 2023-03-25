786ebff297aaba76b7e33127454a646e6e7bb703
marcosmatosteodoro <mp.matos25@gmail.com>
2023-03-25 13:39:22 -0300

    corrigindo bugs

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('welcome');});
 
-
 Route::resources(['categories' => 'App\Http\Controllers\CategoryController']);
 Route::resources(['activity' => 'App\Http\Controllers\ActivityController']);
-Route::resources(['range' => 'App\Http\Controllers\RangeController']);
-Route::resources(['routinies' => 'App\Http\Controllers\RoutiniesController']);
+Route::resources(['rage' => 'App\Http\Controllers\RageController']);
+Route::resources(['routines' => 'App\Http\Controllers\RoutinesController']);
 
