0c78600e6114bd1d9e97b1c9f19ffed7e8244d3c
marcosmatosteodoro <mp.matos25@gmail.com>
2023-03-25 19:42:36 -0300

    fix

--- app/Models/Routines.php
+++ app/Models/Routines.php
@@ -15,12 +15,45 @@ class Routines extends Model
 
     public function getRoutines(): object
     {
-        return self::all();
+        return self::join('activities', 'activity_id', '=', 'activities.id')
+        ->join('status', 'status_id', '=', 'status.id')
+        ->join('categories', 'activities.category_id', 'categories.id')
+        ->leftjoin('rages', 'activities.range_id', 'rages.id')
+        ->select(
+            'routines.id', 
+            'routines.activity_id',
+            'activities.name as activity', 
+            'activities.description', 
+            'routines.status_id',
+            'status.name as status',
+            'routines.date',
+            'activities.range_id',
+            'rages.name as rage',
+            'activities.category_id', 
+            'categories.name as category',
+        )->get();
     }
 
     public function getRoutine(int $id): object
     {
-        return self::find($id);
+        return self::join('activities', 'activity_id', '=', 'activities.id')
+        ->join('status', 'status_id', '=', 'status.id')
+        ->join('categories', 'activities.category_id', 'categories.id')
+        ->leftjoin('rages', 'activities.range_id', 'rages.id')
+        ->select(
+            'routines.id', 
+            'routines.activity_id',
+            'activities.name as activity', 
+            'activities.description', 
+            'routines.status_id',
+            'status.name as status',
+            'routines.date',
+            'activities.range_id',
+            'rages.name as rage',
+            'activities.category_id', 
+            'categories.name as category',
+        )->where('routines.id', $id)
+        ->get();
     }
 
     public function createRoutine(int $activity_id, int $status_id, string $date): bool
