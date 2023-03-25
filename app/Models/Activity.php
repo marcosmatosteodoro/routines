19e455f09ba69af180f34c3cf551188c1d0856bf
marcosmatosteodoro <mp.matos25@gmail.com>
2023-03-25 12:37:47 -0300

    models

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -5,20 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Activity extends Model
 {
     use HasFactory;
 
-    protected $table = 'categories';
+    use HasFactory;
+
+    protected $table = 'activities';
 
     // para tirar a opção de created_at updated_at das tabelas
-    public $timestamps = false;
+    // public $timestamps = false;
 
     // aqui a solução para inserir produtos com status default 'A' sem precisar ter ele no form
     // protected $attributes = ['status' => 'A'];
     
     // campos que serão preenchidos pelo Request::all()
-    protected $fillable = array('name');
+    protected $fillable = array('name', 'description', 'category_id');
 
    // campos que serão exclusivo e não serão aceitos via request get ou post
     // protected $guarded = ['id'];
